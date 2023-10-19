18392e3774695ac1f47ff60ef2672ca2bb4fd7c5
Farahsaiza <farahsaiza@gmail.com>
2023-10-19 21:39:38 +0100

    Add files via upload

---
+++ child age.py
@@ -0,0 +1,11 @@
+Age=int(input("enter the child's age :"))
+if Age==6 or Age==7:
+    print("poussin")
+elif Age==8 or Age==9:
+    print("pupille")
+elif Age==10 or Age==11:
+    print("minie")
+elif Age>=12:
+    print("cadet")
+else:
+    print("this age is not included")
